a5deb83d8959e2878fc8379900b36270064110e6
DWE-CLOUD <85799123+DWE-CLOUD@users.noreply.github.com>
2024-09-08 22:52:20 +0530

    Front : Landing Added

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,18 @@
-import Image from "next/image";
+import React from 'react';
+import Header from './components/Header';
+import Introduction from './components/Introduction';
+import History from './components/History';
 
-export default function Home() {
-  return (
-    <button className="p-[3px] relative">
-      <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-      <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-        Lit up borders
-      </div>
-    </button>
-  );
-}
+const Home: React.FC = () => {
+    return (
+        <div className="min-h-screen bg-gradient-to-b from-[#1E0D2D] to-[#0D0513] text-white font-sans">
+            <Header />
+            <main className="container mx-auto px-4 py-8">
+                <Introduction />
+                <History />
+            </main>
+        </div>
+    );
+};
+
+export default Home;
