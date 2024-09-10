ff695b6183b04faf29de7ae9c8cdcb0f94e3bc8d
alvinbengeorge <alvinallen333@gmail.com>
2024-09-10 23:57:27 +0530

    feat: add basic aceternity form

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
+import Form from "../components/domains/form";
 
 export default function Home() {
   return (
-    <button className="p-[3px] relative">
-      <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-      <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-        Lit up borders
-      </div>
-    </button>
+    <div className="grid place-items-center h-screen">
+      <Form />
+    </div>
   );
 }
