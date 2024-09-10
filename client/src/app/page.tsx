9f57fdd67d59d9224feeae25a06704696b161cc9
alvinbengeorge <alvinallen333@gmail.com>
2024-09-11 00:49:29 +0530

    feat: layout for form

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,7 +4,12 @@ import Form from "../components/domains/form";
 export default function Home() {
   return (
     <div className="grid place-items-center h-screen">
-      <Form />
+      <Form
+        includeGithub={true}
+        domain={"technical"}
+        subdomains={["frontend", "backend"]}
+        description="Some description about the domain"
+      />
     </div>
   );
 }
--- client/src/components/domains/form.tsx
+++ client/src/components/domains/form.tsx
@@ -1,10 +1,20 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { cn } from "@/lib/utils";
 
-export default function SignupFormDemo() {
+export default function Form({
+  includeGithub,
+  domain,
+  subdomains,
+  description,
+}: {
+  includeGithub: boolean;
+  domain: string;
+  subdomains: string[];
+  description: string;
+}) {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("Form submitted");
@@ -12,39 +22,102 @@ export default function SignupFormDemo() {
   return (
     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-        Text1
+        {domain.toUpperCase()}
       </h2>
       <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-        Some description here
+        {description}
       </p>
 
       <form className="my-8" onSubmit={handleSubmit}>
         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
           <LabelInputContainer>
-            <Label htmlFor="firstname">First name</Label>
-            <Input id="firstname" placeholder="Tyler" type="text" />
+            {/* <Label htmlFor="firstname">First name</Label> */}
+            <Input id="firstname" placeholder="First name" type="text" />
           </LabelInputContainer>
           <LabelInputContainer>
-            <Label htmlFor="lastname">Last name</Label>
-            <Input id="lastname" placeholder="Durden" type="text" />
+            {/* <Label htmlFor="lastname">Last name</Label> */}
+            <Input id="lastname" placeholder="Last name" type="text" />
           </LabelInputContainer>
         </div>
+        {/*
+        firstName, lastName, registrationNumber, srmEmail, personalEmail, phoneNumber, department, year, github, linkedin, subDomain
+        */}
         <LabelInputContainer className="mb-4">
-          <Label htmlFor="email">Email Address</Label>
-          <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
-        </LabelInputContainer>
-        <LabelInputContainer className="mb-4">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" placeholder="••••••••" type="password" />
-        </LabelInputContainer>
-        <LabelInputContainer className="mb-8">
-          <Label htmlFor="twitterpassword">Your twitter password</Label>
+          {/* <Label htmlFor="registrationNumber">Registration number</Label> */}
           <Input
-            id="twitterpassword"
-            placeholder="••••••••"
-            type="twitterpassword"
+            id="registrationNumber"
+            placeholder="Registration Number"
+            type="text"
           />
         </LabelInputContainer>
+        <LabelInputContainer className="mb-4">
+          {/* <Label htmlFor="srmEmail">SRM email</Label> */}
+          <Input id="srmEmail" placeholder="SRM Email ID" type="email" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-4">
+          {/* <Label htmlFor="personalEmail">Personal email</Label> */}
+          <Input id="personalEmail" placeholder="Email" type="email" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-4">
+          {/* <Label htmlFor="phoneNumber">Phone number</Label> */}
+          <Input id="phoneNumber" placeholder="Phone Number" type="tel" />
+        </LabelInputContainer>
+        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+          <LabelInputContainer className="">
+            {/* <Label htmlFor="department">Department</Label> */}
+            <Input id="department" placeholder="Department" type="text" />
+          </LabelInputContainer>
+          <LabelInputContainer className="">
+            {/* <Label htmlFor="year">Year</Label> */}
+            <Input id="year" placeholder="Which year btw?" type="number" />
+          </LabelInputContainer>
+        </div>
+        {includeGithub && (
+          <div>
+            <LabelInputContainer className="mb-4">
+              {/* <Label htmlFor="github">Github</Label> */}
+              <Input
+                id="github"
+                placeholder="github.com/username"
+                type="text"
+              />
+            </LabelInputContainer>
+            <LabelInputContainer className="mb-4">
+              {/* <Label htmlFor="linkedin">Linkedin</Label> */}
+              <Input
+                id="linkedin"
+                placeholder="linkedin.com/in/username"
+                type="text"
+              />
+            </LabelInputContainer>
+          </div>
+        )}
+        <LabelInputContainer className="mb-4">
+          {/* <Label htmlFor="subDomain">Subdomain</Label> */}
+          <select
+            id="cars"
+            name="cars"
+            className={cn(
+              `flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent 
+          file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
+          focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
+           disabled:cursor-not-allowed disabled:opacity-50
+           dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
+           group-hover/input:shadow-none transition duration-400
+           `
+            )}
+          >
+            {subdomains.map((subdomain, index) => {
+              return (
+                <option value={subdomain} key={index}>
+                  {subdomain}
+                </option>
+              );
+            })}
+          </select>
+        </LabelInputContainer>
+
+        <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-2 h-[1px] w-full" />
 
         <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
@@ -53,8 +126,6 @@ export default function SignupFormDemo() {
           Submit &rarr;
           <BottomGradient />
         </button>
-
-        <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
       </form>
     </div>
   );
