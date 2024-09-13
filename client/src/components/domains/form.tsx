1ee6bda56ac93f5cca6a5e59e927894ecfcebbbf
alvinbengeorge <alvinallen333@gmail.com>
2024-09-13 10:34:39 +0530

    feat: required fields

--- client/src/components/domains/form.tsx
+++ client/src/components/domains/form.tsx
@@ -24,10 +24,24 @@ export default function Form({
   const [year, setYear] = useState("");
   const [github, setGithub] = useState("");
   const [linkedin, setLinkedin] = useState("");
-  const [subDomain, setSubDomain] = useState("");
+  const [subDomain, setSubDomain] = useState(subdomains[0]);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (
+      !firstname ||
+      !registrationNumber ||
+      !srmEmail ||
+      !personalEmail ||
+      !phoneNumber ||
+      !department ||
+      !year ||
+      (includeGithub && (!github || !linkedin)) || 
+      !subDomain
+    ) {
+      alert("Please fill in all required fields.");
+      return;
+    }
     console.log("Form submitted");
     const data = {
       firstname,
@@ -157,6 +171,7 @@ export default function Form({
           <select
             id="cars"
             name="cars"
+            value={subDomain}
             className={cn(
               `flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent 
           file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
