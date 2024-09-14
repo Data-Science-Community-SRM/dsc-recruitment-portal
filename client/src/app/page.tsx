f1ae3b243c11b89c437a488c344cdf9ceaded665
DWE-CLOUD <85799123+DWE-CLOUD@users.noreply.github.com>
2024-09-15 01:03:02 +0530

    feat: Fixed page and Added Domain Cards

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,24 +2,25 @@ import React from "react";
 import Header from "@/components/home/header";
 import Introduction from "@/components/home/introduction";
 import History from "@/components/home/history";
-import Form from "@/components/domains/form";
+import Hero from "@/components/home/hero";
+import Domain from "@/components/home/domain-cards";
 
 const Home: React.FC = () => {
-  return (
-    <div className="min-h-screen bg-gradient-to-b from-[#1E0D2D] to-[#0D0513] text-white font-sans">
-      <Header />
-      <main className="container mx-auto px-4 py-8">
-        <Introduction />
-        <History />
-        <Form
-          includeGithub={true}
-          domain="technical"
-          subdomains={["web", "app", "design"]}
-          description="Fill the form to register for the technical domain"
-        />
-      </main>
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-gradient-to-b from-[#1E0D2D] to-[#0D0513] text-white font-sans">
+            <Header />
+            <main className="container mx-auto px-4 py-8">
+                <Introduction />
+                <History />
+                <div className="mt-36">
+                    <Domain />
+                </div>
+
+                <Hero />
+
+            </main>
+        </div>
+    );
 };
 
 export default Home;
