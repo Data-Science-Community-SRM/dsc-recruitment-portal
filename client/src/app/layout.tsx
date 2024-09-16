df928a76676b66ac43ce8a8335815a1cfa4d37a7
zydnet <da9592@srmist.edu.in>
2024-09-16 16:51:24 +0530

    fix build issue

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Footer from "@/components/footer";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
